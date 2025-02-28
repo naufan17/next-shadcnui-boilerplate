aa2e63db237c5682a4b4e7f21001ee2a9e58c213
Muhammad Naufan <muhammadnaufan719@yahoo.com>
2025-03-01 01:57:59 +0700

    feat: make product list page

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,15 +1,132 @@
 import AdminLayout from "@/components/layout/admin-layout";
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger, DialogContent } from "@/components/ui/dialog";
+import { ChevronDown } from "lucide-react";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuLabel, DropdownMenuTrigger, DropdownMenuCheckboxItem } from "@/components/ui/dropdown-menu";
+import { TableProduct } from "@/components/table-product";
+
+const products = {
+  data: [
+    {
+      id: 1000001,
+      name: "Asus ROG",
+      description: "15.6 inch, RAM 32GB, SSD 1TB",
+      category: "Laptop",
+      price: 'Rp. 20.000.00',
+    },
+    {
+      id: 1000002,
+      name: "Asus Zenbook",
+      description: "14 inch, RAM 132GB, SSD 1TB",
+      category: "Laptop",
+      price: 'Rp. 18.000.00',
+    },
+    {
+      id: 1000003,
+      name: "Asus VivoBook",
+      description: "14 inch, RAM 16GB, SSD 1TB",
+      category: "Laptop",
+      price: 'Rp. 15.000.00',
+    },
+    {
+      id: 1000004,
+      name: "Lenovo IdeaPad",
+      description: "14 inch, RAM 16GB, SSD 1TB",
+      category: "Laptop",
+      price: 'Rp. 13.000.00',
+    },
+    {
+      id: 1000005,
+      name: "Lenovo Yoga",
+      description: "14.5 inch, RAM 32GB, SSD 1TB",
+      category: "Laptop",
+      price: 'Rp. 24.000.00',
+    },
+    {
+      id: 1000006,
+      name: "Lenovo Legion",
+      description: "15.6 inch, RAM 32GB, SSD 1TB",
+      category: "Laptop",
+      price: 'Rp. 25.000.00',
+    },
+    {
+      id: 1000007,
+      name: "Acer Aspire",
+      description: "14 inch, RAM 16GB, SSD 512GB",
+      category: "Laptop",
+      price: 'Rp. 10.000.00',
+    },
+    {
+      id: 1000008,
+      name: "Acer Predator",
+      description: "15.6 inch, RAM 32GB, SSD 1TB",
+      category: "Laptop",
+      price: 'Rp. 20.000.00',
+    },
+  ],
+}
 
 export default function Page() {
   return (
     <AdminLayout>
-      <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-        <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-          <div className="aspect-video rounded-xl bg-muted/50" />
-          <div className="aspect-video rounded-xl bg-muted/50" />
-          <div className="aspect-video rounded-xl bg-muted/50" />
+      <div className="flex flex-col p-4 pt-0 w-full">
+        <div className="flex flex-row justify-between">
+          <Input type="search" placeholder="Find product..." className="w-1/3" />
+          <div className="space-x-2">
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant="outline">
+                  Filter
+                  <ChevronDown className="ml-2 h-4 w-4" />
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end">
+                <DropdownMenuLabel>Category</DropdownMenuLabel>
+                <DropdownMenuCheckboxItem className="align-left">Tablet</DropdownMenuCheckboxItem>
+                <DropdownMenuCheckboxItem className="align-left">Laptop</DropdownMenuCheckboxItem>
+                <DropdownMenuCheckboxItem className="align-left">Smartphone</DropdownMenuCheckboxItem>
+                <DropdownMenuCheckboxItem className="align-left">Personal Computer</DropdownMenuCheckboxItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+            <Dialog>
+              <DialogTrigger asChild>
+                <Button variant="default">Add Product</Button>
+              </DialogTrigger>
+              <DialogContent>
+                <DialogHeader>
+                  <DialogTitle>Add Product</DialogTitle>
+                  <DialogDescription>Create a new product here. Click save when you&apos;re done.</DialogDescription>
+                </DialogHeader>
+                <form>
+                  <div className="flex flex-col gap-6">
+                    <div className="grid gap-2">
+                      <Label htmlFor="name">Name</Label>
+                      <Input id="name" type="text"/>
+                    </div>
+                    <div className="grid gap-2">
+                      <Label htmlFor="description">Description</Label>
+                      <Input id="description" type="text"/>
+                    </div>
+                    <div className="grid gap-2">
+                      <Label htmlFor="category">Category</Label>
+                      <Input id="category" type="text"/>
+                    </div>
+                    <div className="grid gap-2">
+                      <Label htmlFor="price">Price</Label>
+                      <Input id="price" type="text"/>
+                    </div>
+                  </div>
+                </form>
+                <DialogFooter>
+                  <Button type="submit">Save changes</Button>
+                </DialogFooter>
+              </DialogContent>
+            </Dialog>
+          </div>
         </div>
-        <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
+        <TableProduct products={products} /> 
       </div>
     </AdminLayout>
   )
---
+++ src/components/table-product.tsx
@@ -0,0 +1,107 @@
+import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger, AlertDialogContent } from "@/components/ui/alert-dialog";
+import { Dialog, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger, DialogContent } from "@/components/ui/dialog";
+import { Badge } from "@/components/ui/badge";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { Delete, Edit } from "lucide-react";
+import { Button } from "./ui/button";
+import { Label } from "./ui/label";
+import { Input } from "./ui/input";
+
+interface TableProductProps {
+  products: {
+    data: {
+      id: number;
+      name: string;
+      description: string;
+      category: string;
+      price: string;
+    }[]
+  }
+}
+
+export function TableProduct({ products }: TableProductProps) {
+  return (
+    <div className="rounded-md border w-full mt-4">
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead>ID</TableHead>
+          <TableHead>Name</TableHead>
+          <TableHead>Description</TableHead>
+          <TableHead>Category</TableHead>
+          <TableHead>Price</TableHead>
+          <TableHead>Actions</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {products.data.map((product) => (
+          <TableRow key={product.id} className="py-2">
+            <TableCell className="font-medium">{product.id}</TableCell>
+            <TableCell>{product.name}</TableCell>
+            <TableCell>{product.description}</TableCell>
+            <TableCell><Badge variant="secondary">{product.category}</Badge></TableCell>
+            <TableCell>{product.price}</TableCell>
+              <TableCell>
+                <Dialog>
+                  <DialogTrigger>
+                    <Button variant="ghost" className="p-1.5 h-auto mr-1">
+                      <Edit className="h-4 w-4" />
+                    </Button>
+                  </DialogTrigger>
+                  <DialogContent>
+                    <DialogHeader>
+                      <DialogTitle>Edit Product</DialogTitle>
+                      <DialogDescription>Make changes to your product here. Click save when you&apos;re done.</DialogDescription>
+                    </DialogHeader>
+                    <form>
+                      <div className="flex flex-col gap-6">
+                        <div className="grid gap-2">
+                          <Label htmlFor="name">Name</Label>
+                          <Input id="name" type="text" defaultValue={product.name} />
+                        </div>
+                        <div className="grid gap-2">
+                          <Label htmlFor="description">Description</Label>
+                          <Input id="description" type="text" defaultValue={product.description} />
+                        </div>
+                        <div className="grid gap-2">
+                          <Label htmlFor="category">Category</Label>
+                          <Input id="category" type="text" defaultValue={product.category} />
+                        </div>
+                        <div className="grid gap-2">
+                          <Label htmlFor="price">Price</Label>
+                          <Input id="price" type="text" defaultValue={product.price} />
+                        </div>
+                      </div>
+                    </form>
+                    <DialogFooter>
+                      <Button type="submit">Save changes</Button>
+                    </DialogFooter>
+                  </DialogContent>
+                </Dialog>
+                <AlertDialog>
+                  <AlertDialogTrigger>
+                    <Button variant="ghost" className="p-1.5 h-auto">
+                      <Delete className="h-4 w-4" />
+                    </Button>
+                  </AlertDialogTrigger>
+                  <AlertDialogContent>
+                    <AlertDialogHeader>
+                      <AlertDialogTitle>Are you sure?</AlertDialogTitle>
+                      <AlertDialogDescription>
+                        This action cannot be undone. This will permanently delete your product.
+                      </AlertDialogDescription>
+                    </AlertDialogHeader>
+                    <AlertDialogFooter>
+                      <AlertDialogCancel>Cancel</AlertDialogCancel>
+                      <AlertDialogAction>Continue</AlertDialogAction>
+                    </AlertDialogFooter>
+                  </AlertDialogContent>
+                </AlertDialog>
+              </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  </div>
+  )
+}
